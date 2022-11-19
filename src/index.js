0a39d1f88ea87dff7187860e3b37ca7b8b6d6778
Pratham <prathamsinghal2003@gmail.com>
2022-11-19 15:47:58 +0530

    added greet functionality in indexjs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-require('dotenv').config
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
+
+const token = process.env.TOKEN;
+// console.log(token);
+const bot = new TelegramBot(token, {polling: true});
+bot.on('message',(msg)=>{
+    var hi = "hi";
+    if (msg.text.toString().toLowerCase().indexOf(hi) === 0) {
+    bot.sendMessage(msg.chat.id,"Hello dear user");
+    console.log(msg)
+    }
+    var bye= "bye";
+    if(msg.text.toString().toLowerCase().includes(bye)){
+        bot.sendMessage(msg.chat.id,"Hope to see you around again, "+msg.from.first_name)
+    }
+
+})
